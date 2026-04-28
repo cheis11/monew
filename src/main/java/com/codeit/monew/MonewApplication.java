420af7cfd05428b9e76592d1d0047493556f020c
hochan <106953872+cheis11@users.noreply.github.com>
2026-04-28 19:25:31 +0900

    feat(사용자 관리): 공통 삭제 일자 필드 추가 및 User 엔티티 Soft Delete 적용

--- src/main/java/com/codeit/monew/MonewApplication.java
+++ src/main/java/com/codeit/monew/MonewApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@org.springframework.scheduling.annotation.EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 public class MonewApplication {
